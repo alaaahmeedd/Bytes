e1a12011aaf6f0d7ddc54e4759496f502a531b5c
alaaahmeedd <alaa.aahmeedd@gmail.com>
2022-12-19 05:13:05 -0800

    create project bytes

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email',125)->unique();
+            $table->string('phone',50)->unique(); 
+            $table->string('address',200);
+            $table->string('password',100);
+            $table->string('company',50);
+            $table->string('title',50);
+            $table->string('token',100);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
