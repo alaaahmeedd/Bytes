27e86bd76161f1cd684a0d55187a7ef4e6aac035
alaaahmeedd <alaa.aahmeedd@gmail.com>
2022-12-19 05:21:23 -0800

    create table admins

---
+++ database/migrations/2022_12_19_131739_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email',125)->unique();
+            $table->string('phone',50)->unique();
+            $table->string('password',100);
+            $table->string('token',100);
+            $table->date('Date');
+            $table->date('Update');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
